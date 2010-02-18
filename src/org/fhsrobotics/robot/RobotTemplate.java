665a04d0aabb06fb9fc6ea8b5e0514e5321034b4
Ben <spicyjalapeno@gmail.com>
2010-02-17 18:28:09 -0700

    Retroactivity FTW?

--- src/org/fhsrobotics/robot/RobotTemplate.java
+++ src/org/fhsrobotics/robot/RobotTemplate.java
@@ -1,43 +1,197 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
+/* Copyright (c) FHS 2010. All Rights Reserved.                               */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
+/* the project.   October Pi                                                  */
 /*----------------------------------------------------------------------------*/
 
 package org.fhsrobotics.robot;
 
 
-import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.*;
+
+import edu.wpi.first.wpilibj.camera.AxisCamera;
+import org.fhsrobotics.robot.control.OmniDrive;
+
+
+/*
+
+		PWM
+		1: Front right motor
+		2: Front left motor
+		3: Back right motor
+		4: Back left motor
+		5: Camera servo l/r (unused)
+		6: Camera servo u/d (unused)
+
+		Digital I/O
+		1: Pressure switch
+
+		Relay
+		1: Compressor spike controller
+
+		cRIO digital module
+		1: Solenoid
+		2: Solenoid
+		Solenoid port numbers can be freely swapped.
+
+*/
 
 /**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
+ * The RobotMain is the iterative class that makes the robot operate!
+ *
+ * @author Davis
+ *    @author Davis
  */
-public class RobotTemplate extends IterativeRobot {
+public class RobotTemplate extends IterativeRobot
+{
+	//How the robot moves around.
+	OmniDrive drive;
+
+	Compressor c;
+
+	//Solenoids. Two, because this was meant for only one solenoid valve.
+	Solenoid sol1, sol2;
+
+	//Joystick references.
+	Joystick lJoy, rJoy;
+
+	//The trigger that releases the kicker.
+	Object trigger;
+
+	//The limit switch that triggers when the pneumatics has reached the kicker.
+	DigitalInput limitPneumatic;
+
+
+	//Control vars for the shooter.
+	boolean isCocked;
+	int triggerTimeRemaining;
+	boolean extending;
+
+	boolean inTankDrive;
+	boolean lJoyTriggerLast;
+
+	AxisCamera axis;
+
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
-    public void robotInit() {
+    public void robotInit()
+	{
+		axis = AxisCamera.getInstance();
+		axis.writeResolution(AxisCamera.ResolutionT.k320x240);
 
+		//Initialize everything.
+		c = new Compressor(1,1);
+		c.start();
+
+		sol1 = new Solenoid(7,1);
+		sol2 = new Solenoid(7,2);
+
+		lJoy = new Joystick(1);
+		rJoy = new Joystick(2);
+
+		drive = new OmniDrive(rJoy, lJoy,
+				new Jaguar(1), new Jaguar(2),
+				new Jaguar(3), new Jaguar(4));
+
+		//Initialize the new trigger.
+		trigger = null;
+
+		//Initialize the limit switch
+		//limitPneumatic = new DigitalInput(1);
+
+		isCocked = true;
+		triggerTimeRemaining = 0;
+		extending = false;
+
+		inTankDrive = true;
+
+		lJoyTriggerLast = false;
+
+		
     }
 
     /**
-     * This function is called periodically during autonomous
+     * This function is called periodically during autonomous.
      */
-    public void autonomousPeriodic() {
+    public void autonomousPeriodic()
+	{
+		//Might as well make the robot retract the actuator for real.
+		sol1.set(false);
+		sol2.set(true);
 
+		//And charge the accumulators if need be.
+		if(!c.enabled())
+			c.start();
     }
 
     /**
-     * This function is called periodically during operator control
+     * This function is called periodically during operator control.
      */
-    public void teleopPeriodic() {
-        
+    public void teleopPeriodic()
+	{
+
+
+		//Seconds per loop = 1/getLoopsPerSec();
+		
+		if(lJoy.getTrigger() && !lJoyTriggerLast)
+			inTankDrive = !inTankDrive;
+		lJoyTriggerLast = lJoy.getTrigger();
+
+
+		//Update the drive code.
+		if(inTankDrive)
+			drive.updateTank();
+		else
+			drive.updateIndependant();
+
+		//This might not be necessary, but I added it because I didn't want to
+		//	build it and have such a little thing fail the robot.
+        if(!c.enabled())
+			c.start();
+
+		//Begin kicker control code.
+		/*if(isCocked == true && rJoy.getTrigger())
+		{
+			//trigger.fire();
+			isCocked = false;
+			//Make the code wait 2 seconds before retracting the solenoids.
+			triggerTimeRemaining = (int)(2.0*getLoopsPerSec());
+			extending = true;
+			//Extend the solenoids.
+			sol1.set(true);
+			sol2.set(false);
+		}
+		if(extending == true)
+		{
+			if(triggerTimeRemaining > 0)
+				triggerTimeRemaining--;
+			else if(triggerTimeRemaining <= 0)
+			{
+				//Reverse the solenoids to normal, and set the timer for the next shot.
+				sol1.set(false);
+				sol2.set(true);
+				extending = false;
+				triggerTimeRemaining = (int)(3.0*getLoopsPerSec());
+			}
+		}
+		else
+		{
+			if(triggerTimeRemaining > 0)
+				triggerTimeRemaining--;
+			else if(triggerTimeRemaining <= 0)
+			{
+
+				//The kicker just finished cocking.
+				sol1.set(false);
+				sol2.set(true);
+				extending = false;
+				isCocked = true;
+			}
+		*/
+
     }
-    
+
 }
---
+++ src/org/fhsrobotics/robot/control/OmniDrive.java
@@ -0,0 +1,113 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FHS 2010. All Rights Reserved.                               */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.fhsrobotics.robot.control;
+
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ * @author Davis
+ * 
+ * This class moves the robot in an omni way. It moves as if it's a tank drive,
+ * but moving joysticks sideways makes the robot move sideways the average of
+ * the two joysticks' x movement in magnitude.
+ */
+public class OmniDrive
+{
+	//Left and right joysticks
+	Joystick leftJoy, rightJoy;
+
+	//Jaguar controls.
+	//Front right, front left, back right, back left.
+	Jaguar frJag, flJag, brJag, blJag;
+
+	/**
+	 * Creates a new OmniDrive system with the specified settings.
+	 * @param settings
+	 */
+	public OmniDrive()
+	{
+		//Initialise the joysticks!
+		leftJoy = new Joystick(0);
+		rightJoy = new Joystick(1);
+
+		//Initialise the jaguars!
+		frJag = new Jaguar(1);
+		flJag = new Jaguar(2);
+		brJag = new Jaguar(3);
+		blJag = new Jaguar(4);
+	}
+
+	/**
+	 * Creates an omni drive with the specified joysticks and jaguars.
+	 * @param rJoystick
+	 * @param lJoystick
+	 * @param frJaguar Front right jaguar.
+	 * @param flJaguar Front left jaguar.
+	 * @param brJaguar Back right jaguar.
+	 * @param blJaguar Back left jaguar.
+	 */
+	public OmniDrive(Joystick rJoystick, Joystick lJoystick,
+			Jaguar frJaguar, Jaguar flJaguar,
+			Jaguar brJaguar, Jaguar blJaguar)
+	{
+		//Set the joysticks.
+		leftJoy = lJoystick;
+		rightJoy = rJoystick;
+
+		//Set the jaguars.
+		frJag = frJaguar;
+		flJag = flJaguar;
+		brJag = brJaguar;
+		blJag = blJaguar;
+	}
+
+	/**
+	 * Call this method whenever you want the motors to update with new data
+	 * from the joysticks.
+	 */
+	public void updateTank()
+	{
+		//Get the average value on the X axis of both joysticks.
+		double averageJoyX = (rightJoy.getX()+leftJoy.getX())/2.0;
+
+		//The whole max/min business is to limit the result from -1 to 1.
+
+		//First, set the front jaguars. They both will go forward if the
+		//joysticks are pushed forwards, the right will go backwards if the
+		//joysticks are pushed to the right, and the left will go forwards if
+		//the joysticks are pushed to the right.
+		frJag.set(Math.max(Math.min(rightJoy.getY() + averageJoyX, 1),-1));
+		flJag.set(Math.max(Math.min(leftJoy.getY() - averageJoyX, 1),-1));
+
+		//Then set the rear jaguars. They both will go forward if the
+		//joysticks are pushed forwards, the right will go forwards if the
+		//joysticks are pushed to the right, and the left will go backwards if
+		//the joysticks are pushed to the right.
+		brJag.set(Math.max(Math.min(rightJoy.getY() - averageJoyX, 1),-1));
+		blJag.set(Math.max(Math.min(leftJoy.getY() + averageJoyX, 1),-1));
+	}
+
+	public void updateIndependant()
+	{
+		//This drives the robot, using the L joy for rot, r joy for trans.
+		//lJoy+, robot cw
+		/*frJag.set(Math.max(Math.min(rightJoy.getY() + rightJoy.getX() + leftJoy.getX() + rightJoy.getZ(), 1),-1));
+		flJag.set(Math.max(Math.min(rightJoy.getY() - rightJoy.getX() - leftJoy.getX() - rightJoy.getZ(), 1),-1));
+		brJag.set(Math.max(Math.min(rightJoy.getY() - rightJoy.getX() + leftJoy.getX() + rightJoy.getZ(), 1),-1));
+		blJag.set(Math.max(Math.min(rightJoy.getY() + rightJoy.getX() - leftJoy.getX() - rightJoy.getZ(), 1),-1));*/
+
+		frJag.set(Math.max(Math.min(rightJoy.getY() + rightJoy.getX() + leftJoy.getX(), 1),-1));
+		flJag.set(Math.max(Math.min(rightJoy.getY() - rightJoy.getX() - leftJoy.getX(), 1),-1));
+		brJag.set(Math.max(Math.min(rightJoy.getY() - rightJoy.getX() + leftJoy.getX(), 1),-1));
+		blJag.set(Math.max(Math.min(rightJoy.getY() + rightJoy.getX() - leftJoy.getX(), 1),-1));
+
+		//flJag.set(1);
+		//frJag.set(1);
+	}
+}
